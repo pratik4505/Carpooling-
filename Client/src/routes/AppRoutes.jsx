5eea0eaf2c9635712ed7340cb2560debf025fe40
Kushagra <kushagraverma572@gmail.com>
2024-03-11 23:48:10 +0530

    Added login page

--- Client/src/routes/AppRoutes.jsx
+++ Client/src/routes/AppRoutes.jsx
@@ -16,6 +16,7 @@ import Profile from "../pages/Profile";
 import RideRequests from "../pages/RideRequests";
 import Transactions from "../pages/Transactions";
 import Home from "../pages/Home";
+import Login from "../components/Login/Login";
 
 const AppRoutes = () => {
   return (
@@ -25,7 +26,7 @@ const AppRoutes = () => {
         <Route path="*" element={<h1>Not Found</h1>} />
 
         {/* <Route path=/Register" element={<></>} /> */}
-
+        <></>
         <Route path="/" element={<ProtectedRoute/>}>
         <Route path="/home" element={<Home/>} />
           <Route path="/publishRide" element={<CreateRide />} />
@@ -38,6 +39,7 @@ const AppRoutes = () => {
           <Route path="/profile/:ownerId" element={<Profile />} />
           <Route path="/rideRequests" element={<RideRequests />} />
           <Route path="/transactions" element={<Transactions />} />
+          <Route path="/login" element = {<Login/>}/>
         </Route>
       </Routes>
     </Suspense>
